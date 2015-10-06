2a2aafabe8cbe309b8521ab9d9e8629e9a3d1921
Rameen Shafiee <rs4047@nyu.edu>
2015-10-06 03:01:39 -0400

    1st commit

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public Transform followThis; //assign in Inspector
+	Vector3 inputVector;
+	Rigidbody rbody;
+	
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter () {
+		inputVector = followThis.position - transform.position;
+	
+	
+	//void FixedUpdate () {
+		// "B" - "A" (if you want a vector going from A to B)
+		//inputVector = followThis.position - transform.position;
+		// Normalize "standardizes" a vector to length 1
+		rbody.velocity = Vector3.Normalize ( inputVector ) * 2f;
+		Debug.Log ( rbody.velocity.magnitude );
+		
+	}
+}
---
+++ Assets/Scripts/RigidPlaya.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidPlaya : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey (KeyCode.W) ) {
+			transform.Translate ( 0f, 0f, 0.5f ); // does not care about collision
+		}
+		
+		//go backward
+		if ( Input.GetKey (KeyCode.S) ) {
+			transform.Translate ( 0f, 0f, -0.5f ); // does not respect collision
+		}
+		
+		//float mouseX = Input.GetAxis ( "Mouse X" ); // number from -1 to 1
+		//float mouseY = -Input.GetAxis ( "Mouse Y" );
+		// when your mouse stays still, it will be = 0f
+		// move mouse to right = 1f
+		//move mouse to left = -1f
+		// rotation: X = "pitch", Y = "yaw (left and right), Z = "roll"\
+		if ( Input.GetKey (KeyCode.D ) ) {
+			transform.Rotate ( 0f, 5f, 0f );
+		}
+		if ( Input.GetKey (KeyCode.A ) ) { 
+		transform.Rotate ( 0f, -5f, 0f );
+		}
+	
+	}
+}
